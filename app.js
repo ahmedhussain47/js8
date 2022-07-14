3e783cfe6b8342e9a819857f05ba3ae9c741e684
ahmedhussain47 <68425053+ahmedhussain47@users.noreply.github.com>
2022-07-14 16:00:46 +0500

    javascript assignment 8

---
+++ app.js
@@ -0,0 +1,50 @@
+// var num = +prompt("Enter a Num")
+// //var ceiling1 = Math.ceil(num); //5 
+// document.write(num+"<br>")
+// document.write(Math.round(num)+"<br>")
+// document.write(Math.floor(num)+"<br>")
+// document.write(Math.ceil(num)+"<br>")
+
+// document.write("<br>"+"Q2"+"<br>")
+// var negnum = +prompt("Enter a Negative Num")
+// //var ceiling1 = Math.ceil(negnum); //5 
+// document.write(negnum+"<br>")
+// document.write(Math.round(negnum)+"<br>")
+// document.write(Math.floor(negnum)+"<br>")
+// document.write(Math.ceil(negnum)+"<br>")
+
+// document.write("<br>"+"Q3"+"<br>")
+// var numb = -4;
+// document.write(Math.abs(numb)+"<br>")
+
+// document.write("<br>"+"Q4"+"<br>")
+// var diceRoll = Math.floor( Math.random() * 6 ) +1;
+// document.write('You rolled a ' + diceRoll);
+
+// document.write("<br>"+"Q5"+"<br>")
+// var head = 1;
+// var tail = 2;
+
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss)
+// if(floor === 0){
+//     document.write("Random Coin Value: Head")
+// } else if(floor === 1)
+// {
+//     document.write("Random Coin Value: Tails")
+// }
+
+
+//  document.write("<br>"+"Q6"+"<br>")
+// var ranval = Math.floor( Math.random() * 100 ) +1;
+// document.write('Random value between 1 and 100: ' + ranval);
+
+
+// document.write("<br>"+"Q7"+"<br>")
+// var xval = Math.floor( Math.random() * 10 ) +1;
+// var i = +prompt("enter your num");
+// if (i === x) {
+//     return "congratulations, Good Work";
+//   }else {
+//      return "Not matched";
+//   }
